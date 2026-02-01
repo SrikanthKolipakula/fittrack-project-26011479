a35b698b9a5a2e40377199409b1f58d2de8749e9
DevStudent26011479 <student26011479@university.edu>
2026-02-02 01:19:07 +0530

    created game_score code

---
+++ game_score.py
@@ -0,0 +1,9 @@
+player_name = input("Enter Player Name:")
+no_of_games_played = int(input("Enter No Of Games Played:"))
+total_score = int(input("Enter Total Score:"))
+avg_score = total_score/no_of_games_played
+
+print(f"Player: {player_name}")
+print(f"Games played: {no_of_games_played}")
+print(f"Total score: {total_score}")
+print(f"Average score: {avg_score}")
